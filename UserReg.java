74a42cf148d69bb13c9e1254edc1ed2106660707
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-12 07:53:47 +0530

    Added:to validate password of minimum 8 characters

--- UserReg.java
+++ UserReg.java
@@ -6,6 +6,7 @@ public class UserReg {
     private static final String LAST_NAME="^[A-Z][A-Za-z]{2,}";
     private static final String EMAIL_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
     private static final String MOBILE_PATTERN="^[0-9]{2}[ ][0-9]{10}";
+    private static final String PASSWORD_PATTERN1="^[A-Za-z0-9]{8,}";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern=Pattern.compile(FIRST_NAME);
@@ -22,5 +23,9 @@ public class UserReg {
     public boolean validatePhoneNumber(String phoneNumber) {
         Pattern pattern=Pattern.compile(MOBILE_PATTERN);
         return pattern.matcher(phoneNumber).matches();
+    }
+    public boolean validatePassword(String passsword) {
+        Pattern pattern=Pattern.compile(PASSWORD_PATTERN1);
+        return pattern.matcher(passsword).matches();
     }
 }
