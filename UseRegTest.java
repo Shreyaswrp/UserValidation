9086d06278947cbad46d211bfc55dbfd47787d04
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-12 12:18:07 +0530

    Updated aftr resolving conflict

---
+++ UseRegTest.java
@@ -0,0 +1,41 @@
+
+import com.dummytest.UserReg;
+import org.junit.Assert;
+import org.junit.Test;
+public class UserRegTest {
+
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        UserReg userValidation=new UserReg();
+        boolean result=userValidation.validateFirstName("Shreya");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenFirstName_WhenShort_ShouldReturnFalse() {
+        UserReg userValidation=new UserReg();
+        boolean result=userValidation.validateFirstName("Hello");
+        Assert.assertEquals(false,result);
+
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
+        UserReg userValidation=new UserReg();
+        boolean result=userValidation.validateLastName("Swaroop");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void givenLastName_WhenShort_ShouldReturnFalse() {
+        UserReg userValidation=new UserReg();
+        boolean result=userValidation.validateFirstName("Swrp");
+        Assert.assertEquals(false,result);
+    }
+
+    @Test
+    public void givenEmailId_WhenProper_ShouldReturnTrue() {
+        UserReg userValidation=new UserReg();
+        boolean result=userValidation.validateEmailId("shreyaswrp@gmail.comls");
+        Assert.assertEquals(true,result);
+    }
+}
--- UserReg.java
+++ UserReg.java
@@ -1,3 +1,4 @@
+
 package com.dummytest;
 
 import java.util.regex.Pattern;
@@ -5,6 +6,11 @@ public class UserReg {
     private static final String FIRST_NAME="^[A-Z][A-Za-z]{2,}";
     private static final String LAST_NAME="^[A-Z][A-Za-z]{2,}";
     private static final String EMAIL_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
+    private static final String MOBILE_PATTERN="^[0-9]{2}[ ][0-9]{10}";
+    private static final String PASSWORD_PATTERN1="^[A-Za-z0-9]{8,}";
+    private static final String PASSWORD_PATTERN2="(?=.*[A-Z].*)[A-Za-z0-9]{8,}";
+    private static final String PASSWORD_PATTERN3="(?=.*[A-Z].*)(?=.*[0-9].*)[A-Za-z0-9]{8,}";
+    private static final String PASSWORD_PATTERN4="(?=.*[A-Z].*)(?=.*[0-9].*)([a-zA-Z0-9]{4,}[!@#$%^&*()_+][a-zA-Z0-9]{3,})";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern=Pattern.compile(FIRST_NAME);
@@ -18,4 +24,13 @@ public class UserReg {
         Pattern pattern=Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(emailid).matches();
     }
+    public boolean validatePhoneNumber(String phoneNumber) {
+        Pattern pattern=Pattern.compile(MOBILE_PATTERN);
+        return pattern.matcher(phoneNumber).matches();
+    }
+    public boolean validatePassword(String passsword) {
+        Pattern pattern=Pattern.compile(PASSWORD_PATTERN4);
+        return pattern.matcher(passsword).matches();
+    }
+
 }
