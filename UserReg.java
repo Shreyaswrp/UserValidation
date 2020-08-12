4331b0ac7d789aba31c94a526cbee352f62b4fdf
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-12 07:41:43 +0530

    Added:to validate last name

--- UserReg.java
+++ UserReg.java
@@ -3,9 +3,14 @@ package com.dummytest;
 import java.util.regex.Pattern;
 public class UserReg {
     private static final String FIRST_NAME="^[A-Z][A-Za-z]{2,}";
+    private static final String LAST_NAME="^[A-Z][A-Za-z]{2,}";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern=Pattern.compile(FIRST_NAME);
         return pattern.matcher(firstName).matches();
+    }
+    public boolean validateLastName(String lastName) {
+        Pattern pattern=Pattern.compile(LAST_NAME);
+        return pattern.matcher(lastName).matches();
     }
 }
