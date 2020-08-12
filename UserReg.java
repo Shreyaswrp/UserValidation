16c539e1be67ced467d40cad7b6b099d84e14522
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-12 07:49:56 +0530

    Added:to validate email

--- UserReg.java
+++ UserReg.java
@@ -5,6 +5,7 @@ public class UserReg {
     private static final String FIRST_NAME="^[A-Z][A-Za-z]{2,}";
     private static final String LAST_NAME="^[A-Z][A-Za-z]{2,}";
     private static final String EMAIL_PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$";
+    private static final String MOBILE_PATTERN="^[0-9]{2}[ ][0-9]{10}";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern=Pattern.compile(FIRST_NAME);
@@ -17,5 +18,9 @@ public class UserReg {
     public boolean validateEmailId(String emailid) {
         Pattern pattern=Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(emailid).matches();
+    }
+    public boolean validatePhoneNumber(String phoneNumber) {
+        Pattern pattern=Pattern.compile(MOBILE_PATTERN);
+        return pattern.matcher(phoneNumber).matches();
     }
 }
